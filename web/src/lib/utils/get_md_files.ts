9f3b5712181aab7a7ab548f157ee1ede070ff080
Billiyhills <billiyhills@gmail.com>
2024-10-22 15:30:32 +0530

    style: format

--- web/src/lib/utils/get_md_files.ts
+++ web/src/lib/utils/get_md_files.ts
@@ -1,20 +1,18 @@
-import fs from "fs"
-import { marked } from "marked"
-import path from "path"
+import fs from 'fs';
+import { marked } from 'marked';
+import path from 'path';
 
 export function get_md_files() {
-  const md_dir = path.resolve("../")
-  const md_files = fs.readdirSync(md_dir).filter((file) => file.endsWith(".md"))
+	const md_dir = path.resolve('../');
+	const md_files = fs.readdirSync(md_dir).filter((file) => file.endsWith('.md'));
 
-  const md_res_arr = md_files.map((file) => {
-    const file_absolute_path = path.join(md_dir, file)
-    const file_content = fs.readFileSync(file_absolute_path, "utf8")
+	return md_files.map((file) => {
+		const file_absolute_path = path.join(md_dir, file);
+		const file_content = fs.readFileSync(file_absolute_path, 'utf8');
 
-    return {
-      file_name: file,
-      content: marked.parse(file_content),
-    }
-  })
-
-  return md_res_arr
+		return {
+			file_name: file,
+			content: marked.parse(file_content)
+		};
+	});
 }
